026a09070341749209c74e5700cd73aee34cc76b
lukaspabst <lukas.pabst@outlook.de>
2023-10-27 17:41:00 +0200

    First Set Up

---
+++ demo/src/test/java/com/travelbuddy/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelbuddy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
